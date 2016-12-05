04405a46c86416b06fe22e0bf035c28ac90e8087
WangHaoRan <wanghaoran@horeax.com>
2016-12-05 14:05:29 +0800

    完善首页导航
    将导航栏container-fluid 改为 container

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -59,7 +59,7 @@
 <body>
 
 <nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container-fluid">
+    <div class="container">
 
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
