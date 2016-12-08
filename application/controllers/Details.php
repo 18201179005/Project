00009935f2a66a0c1e324c4d9fe3c21c25d3209c
WangHaoRan <wanghaoran@horeax.com>
2016-12-08 12:00:31 +0800

    详情页面制作
    完成项目详情页制作

---
+++ application/controllers/Details.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Details extends CI_Controller {
+
+    public function web_app(){
+        $this->load->view('details/web_app');
+    }
+
+
+}
---
+++ application/views/details/web_app.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <title>WEB应用系统 - 案例精选 - HOREAX技术支持</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="keywords" content="HOREAX,HOREAX技术支持,HOREAX案例精选,WEB应用系统,北京昊瑞联创科技有限公司"/>
+    <meta name="description" content="HOREAX技术支持是北京昊瑞联创科技有限公司旗下品牌，专注于为客户提供专业、高效、易用的互联网应用技术解决方案" />
+    <meta name="author" content="HOREAX技术支持">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="renderer" content="webkit">
+    <!-- Bootstrap -->
+    <link href="<?=$this->config->base_url()?>public/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?=$this->config->base_url()?>public/css/public.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+    <script src="<?=$this->config->base_url()?>public/js/html5shiv.min.js"></script>
+    <script src="<?=$this->config->base_url()?>public/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<?php $this->load->view('common/header'); ?>
+
+
+<div class="container" style="margin-top: 20px;">
+
+
+    <ol class="breadcrumb">
+
+        <li><a href="#"><span class="glyphicon glyphicon-home"></span> HOREAX案例精选</a></li>
+        <li><a href="#">WEB应用系统</a></li>
+        <li class="active">2017红衫资本联合真格基金携成员企业校园招聘</li>
+    </ol>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">项目简介</div>
+        <div class="panel-body">
+            <img src="<?=$this->config->base_url()?>project/2/1/banner.jpg" width="100%" />
+            <hr/>
+            <p>017红衫资本联合真格基金携成员企业校园招聘阿斯顿上看见啦疯狂巴士的分类及卡布斯的法律框架啊舍不得饭卡就收到发生了</p>
+            <p>017红衫资本联合真格基金携成布斯的法律框架啊舍不得饭卡就收到发生了</p>
+            <strong>测试帐号: <span class="label label-default">13412312322</span> ,密码:123123 </strong>
+        </div>
+    </div>
+
+
+
+
+
+    <div class="panel panel-default">
+        <div class="panel-heading">项目演示地址:</div>
+
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered table-hover">
+                <thead>
+                <tr>
+                    <th>名称</th>
+                    <th>支持终端</th>
+                    <th>访问地址</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>线下宣讲会预约系统</td>
+                        <td>PC</td>
+                        <td><a role="button" class="btn btn-info btn-xs">在线演示</a></td>
+
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+
+    </div>
+
+
+
+
+</div>
+
+
+<?php $this->load->view('common/footer'); ?>
+
+
+
+<script src="<?=$this->config->base_url()?>public/js/jquery.min.js"></script>
+<script src="<?=$this->config->base_url()?>public/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
